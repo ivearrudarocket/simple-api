babf00258eeac971ebac8d0cdde5009b317a7718
ivearrudarocket <ive@rocketmat.com>
2021-11-24 15:21:56 -0300

    primeira api

---
+++ apitest.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+products = [
+    {'id': 1, 'name': 'First Product', 'price': 12.0},
+    {'id': 2, 'name': 'second Product', 'price': 35.0},
+    {'id': 3, 'name': 'third Product', 'price': 30.0}
+]
+
+
+@app.route('/products', methods=['GET'])
+def catalog():
+    return jsonify(products)
+
+
+@app.route('/products', methods=['POST'])
+def create():
+    product = request.json
+    products.append(product)
+
+    return jsonify(product)
+
+
+@app.route('/products/<int:prod_id>', methods=['DELETE'])
+def delete(prod_id):
+    i = 0
+
+    for product in products:
+        if prod_id == product['id']:
+            del products[i]
+
+            return '', 204
+
+        i += 1
+
+    return '', 404
+
+
+@app.route('/products/<int:prod_id>', methods=['PATCH'])
+def to_edit(prod_id):
+    i = 0
+
+    for product in products:
+        if prod_id == product['id']:
+            y = request.json
+            del products[i]
+            products.insert(i, y)
+
+            return '', 200
+
+        i += 1
+
+    return '', 404
